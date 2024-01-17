8ed692661cf75c4646ee58beb59203007b6443b1
ravikajan <61968423+ravikajan@users.noreply.github.com>
2024-01-17 18:26:34 +0530

    fix network interface

--- thermal.cjs
+++ thermal.cjs
@@ -1,23 +1,34 @@
 const {
-  ThermalPrinter,
-  PrinterTypes,
-  CharacterSet,
-  BreakLine,
-} = require("node-thermal-printer");
-
-let printer = new ThermalPrinter({
-  type: PrinterTypes.EPSON, // Printer type: 'star' or 'epson'
-  interface: "printer:EPSON TM-m30II 2", // Use USB interface
-  characterSet: CharacterSet.PC852_LATIN2, // Printer character set
-  removeSpecialCharacters: false, // Removes special characters - default: false
-  lineCharacter: "=", // Set character for lines - default: "-"
-  breakLine: BreakLine.WORD, // Break line after WORD or CHARACTERS. Disabled with NONE - default: WORD
-  options: {
-    // Additional options
-    timeout: 5000, // Connection timeout (ms) [applicable only for network printers] - default: 3000
-  },
-});
-
-console.log("status", await printer.isPrinterConnected());
-
-// Now, you can continue with your printing logic using the 'printer' object
+    ThermalPrinter,
+    PrinterTypes,
+    CharacterSet,
+    BreakLine,
+  } = require("node-thermal-printer");
+  
+  async function checkPrinterStatus() {
+    try {
+      let printer = new ThermalPrinter({
+        type: PrinterTypes.EPSON,
+        interface: "tcp://192.168.0.99:9100",
+        characterSet: CharacterSet.PC852_LATIN2,
+        removeSpecialCharacters: false,
+        lineCharacter: "=",
+        breakLine: BreakLine.WORD,
+        options: {
+          timeout: 5000,
+        },
+      });
+  
+      const status = await printer.isPrinterConnected();
+  
+      console.log("Printer status:", status);
+  
+      // Continue with your printing logic using the 'printer' object if needed
+    } catch (error) {
+      console.error("Error checking printer status:", error);
+    }
+  }
+  
+  // Call the asynchronous function
+  checkPrinterStatus();
+  
