fa9bbdf2627ad9adbd17ec495692b3f40b67d1a8
ravikajan <61968423+ravikajan@users.noreply.github.com>
2024-01-17 17:58:21 +0530

    fixed

---
+++ thermal.cjs
@@ -0,0 +1,23 @@
+const {
+  ThermalPrinter,
+  PrinterTypes,
+  CharacterSet,
+  BreakLine,
+} = require("node-thermal-printer");
+
+let printer = new ThermalPrinter({
+  type: PrinterTypes.STAR, // Printer type: 'star' or 'epson'
+  interface: "USB", // Use USB interface
+  characterSet: CharacterSet.PC852_LATIN2, // Printer character set
+  removeSpecialCharacters: false, // Removes special characters - default: false
+  lineCharacter: "=", // Set character for lines - default: "-"
+  breakLine: BreakLine.WORD, // Break line after WORD or CHARACTERS. Disabled with NONE - default: WORD
+  options: {
+    // Additional options
+    timeout: 5000, // Connection timeout (ms) [applicable only for network printers] - default: 3000
+  },
+});
+
+console.log("status", await printer.isPrinterConnected());
+
+// Now, you can continue with your printing logic using the 'printer' object
