f700250e14cd180dfc0e5b689597f77fbd937289
ravikajan <61968423+ravikajan@users.noreply.github.com>
2024-01-17 18:32:48 +0530

    fix

--- thermal.cjs
+++ thermal.cjs
@@ -1,45 +1,45 @@
 const {
-    ThermalPrinter,
-    PrinterTypes,
-    CharacterSet,
-    BreakLine,
-  } = require("node-thermal-printer");
-  
-  async function checkPrinterStatus() {
-    try {
-      let printer = new ThermalPrinter({
-        type: PrinterTypes.EPSON,
-        interface: "printer:EPSON TM-m30II 2",
-        characterSet: CharacterSet.PC852_LATIN2,
-        removeSpecialCharacters: false,
-        lineCharacter: "=",
-        breakLine: BreakLine.WORD,
-        options: {
-          timeout: 5000,
-        },
-      });
-  
-      const status = await printer.isPrinterConnected();
-  
-      console.log("Printer status:", status);
-  
-      if (status) {
-        // Print "Hello, World!" message
-        printer.println("Hello, World!");
-  
-        // Cut the paper
-        printer.cut();
-  
-        // Execute print job
-        const response = await printer.execute();
-  
-        console.log("Print job response:", response);
-      }
-    } catch (error) {
-      console.error("Error checking printer status:", error);
+  ThermalPrinter,
+  PrinterTypes,
+  CharacterSet,
+  BreakLine,
+} = require("node-thermal-printer");
+
+async function checkPrinterStatus() {
+  try {
+    let printer = new ThermalPrinter({
+      type: PrinterTypes.EPSON,
+      interface: "tcp://192.168.0.9:9100",
+      characterSet: CharacterSet.PC852_LATIN2,
+      removeSpecialCharacters: false,
+      lineCharacter: "=",
+      breakLine: BreakLine.WORD,
+      options: {
+        timeout: 5000,
+      },
+    });
+
+    const status = await printer.isPrinterConnected();
+
+    console.log("Printer status:", status);
+    if (status) {
+      // Print "Hello, World!" message
+      printer.println("Hello, World!");
+
+      // Cut the paper
+      printer.cut();
+
+      // Execute print job
+      const response = await printer.execute();
+
+      console.log("Print job response:", response);
     }
+
+    // Continue with your printing logic using the 'printer' object if needed
+  } catch (error) {
+    console.error("Error checking printer status:", error);
   }
-  
-  // Call the asynchronous function
-  checkPrinterStatus();
-  
+}
+
+// Call the asynchronous function
+checkPrinterStatus();
