d4b7e12fead7dccf04661fccd0b9fc14612ace4b
ravikajan <61968423+ravikajan@users.noreply.github.com>
2024-01-17 18:38:51 +0530

    fix

--- thermal.cjs
+++ thermal.cjs
@@ -23,18 +23,52 @@ async function checkPrinterStatus() {
 
     console.log("Printer status:", status);
     if (status) {
-      printer.alignLeft();
-      printer.newLine();
-      printer.println("Hello World!");
-      printer.println(
-        "This is a long line that will be collapsed into two lines"
-      );
-      printer.drawLine();
-
-      printer.upsideDown(true);
-      printer.println("Hello World upside down!");
-      printer.upsideDown(false);
-      printer.drawLine();
+        printer.alignLeft();
+        printer.newLine();
+        printer.println('Hello World!');
+        printer.println('This is a long line that will be collapsed into two lines');
+        printer.drawLine();
+      
+        printer.upsideDown(true);
+        printer.println('Hello World upside down!');
+        printer.upsideDown(false);
+        printer.drawLine();
+      
+        printer.invert(true);
+        printer.println('Hello World inverted!');
+        printer.invert(false);
+        printer.drawLine();
+      
+        printer.println('Special characters: ČčŠšŽžĐđĆćßẞöÖÄäüÜé');
+        printer.drawLine();
+      
+        printer.setTypeFontB();
+        printer.println('Type font B');
+        printer.setTypeFontA();
+        printer.println('Type font A');
+        printer.drawLine();
+      
+        printer.alignLeft();
+        printer.println('This text is on the left');
+        printer.alignCenter();
+        printer.println('This text is in the middle');
+        printer.alignRight();
+        printer.println('This text is on the right');
+        printer.alignLeft();
+        printer.drawLine();
+      
+        printer.setTextDoubleHeight();
+        printer.println('This is double height');
+        printer.setTextDoubleWidth();
+        printer.println('This is double width');
+        printer.setTextQuadArea();
+        printer.println('This is quad');
+        printer.setTextSize(7, 7);
+        printer.println('Wow');
+        printer.setTextSize(0, 0);
+        printer.setTextNormal();
+        printer.println('This is normal');
+        printer.drawLine();
 
       // Cut the paper
       printer.cut();
