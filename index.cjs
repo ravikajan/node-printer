07c4c3de41207ac2dbb32bbfdcc37fa70c6d4582
ravikajan <61968423+ravikajan@users.noreply.github.com>
2024-01-17 18:53:25 +0530

    fix

--- index.cjs
+++ index.cjs
@@ -1,8 +1,9 @@
-const escpos = require("escpos");
-// install escpos-usb adapter module manually
-escpos.USB = require("escpos-usb");
-// Select the adapter based on your printer type
-const device = new escpos.USB();
+const escpos = require('escpos');
+escpos.Bluetooth = require('escpos-bluetooth');
+
+const address = '00:01:90:66:09:FB';
+const channel = 1;
+const bluetoothDevice = new escpos.Bluetooth(address, channel);
 // const device  = new escpos.Network('localhost');
 // const device  = new escpos.Serial('/dev/usb/lp0');
 
@@ -11,28 +12,28 @@ console.log(device)
 const options = { encoding: "GB18030" /* default */ };
 // encoding is optional
 
-const printer = new escpos.Printer(device, options);
+const printer = new escpos.Printer(bluetoothDevice, options);
 
-// device.open(function (error) {
-//   printer
-//     .font("a")
-//     .align("ct")
-//     .style("bu")
-//     .size(1, 1)
-//     .text("The quick brown fox jumps over the lazy dog")
-//     .text("敏捷的棕色狐狸跳过懒狗")
-//     .barcode("1234567", "EAN8")
-//     .table(["One", "Two", "Three"])
-//     .tableCustom(
-//       [
-//         { text: "Left", align: "LEFT", width: 0.33, style: "B" },
-//         { text: "Center", align: "CENTER", width: 0.33 },
-//         { text: "Right", align: "RIGHT", width: 0.33 },
-//       ],
-//       { encoding: "cp857", size: [1, 1] } // Optional
-//     )
-//     .qrimage("https://github.com/song940/node-escpos", function (err) {
-//       this.cut();
-//       this.close();
-//     });
-// });
+device.open(function (error) {
+  printer
+    .font("a")
+    .align("ct")
+    .style("bu")
+    .size(1, 1)
+    .text("The quick brown fox jumps over the lazy dog")
+    .text("敏捷的棕色狐狸跳过懒狗")
+    .barcode("1234567", "EAN8")
+    .table(["One", "Two", "Three"])
+    .tableCustom(
+      [
+        { text: "Left", align: "LEFT", width: 0.33, style: "B" },
+        { text: "Center", align: "CENTER", width: 0.33 },
+        { text: "Right", align: "RIGHT", width: 0.33 },
+      ],
+      { encoding: "cp857", size: [1, 1] } // Optional
+    )
+    .qrimage("https://github.com/song940/node-escpos", function (err) {
+      this.cut();
+      this.close();
+    });
+});
